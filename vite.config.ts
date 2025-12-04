85eec9466535ff26812a82a9d7a4c1e190436b26
krishnapriyak765 <krishnapriyak765@gmail.com>
2025-12-04 22:32:12 +0530

    Deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/<repo-name>/", // 👈 REQUIRED for GitHub Pages
+  base: "/iest/", // 👈 required for GitHub Pages
 
   server: {
     host: "::",
